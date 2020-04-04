eb60fada9163913d8fd6d3af231d5f4fcb92cb8e
ringi <mitya.info@gmail.com>
2020-04-04 20:25:53 +0300

    обнова2

--- task2_less3_GDA.js
+++ task2_less3_GDA.js
@@ -145,25 +145,111 @@
 // }
 
 // - За допомогою 2х  циклів проітеррувати  даний масив і масив кожного об'єкта.
-let users = [{name: 'vasya', age: 31, status: false, skills: ['java', 'js']}, {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}];
+//  let users = [{name: 'vasya', age: 31, status: false, skills: ['java', 'js']}, {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}];
 
-console.log(users);
-console.log('__________');
-for (const user of users) {
-    // console.log(user)
-    // console.log('__________');
+//  console.log(users);
+//  console.log('__________');
+// for (const user of users) {
+//     // console.log(user)
+//     // console.log('__________');
 
 
-    for (const znach in user) {
+//         for (const znach in user) {
 
-        // console.log( typeof user[znach]);
+//             // console.log( typeof user[znach]);
 
-        if ((typeof user[znach] ==='object') && Array.isArray(user[znach])){
-            for (const arr of user[znach]) {
-                console.log(arr)
-            }
-        }
-    }
+//             if ((typeof user[znach] ==='object') && Array.isArray(user[znach])){
+//                 for (const arr of user[znach]) {
+//                      console.log(arr)
+//                 }
+//             }
+//     }
+
+// }
+
+// З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+// let obj=[];
+// //  console.log(users);
+//  for (let i = 0; i < users.length; i++) {
+//       const user = users[i];
+//         // console.log(user.address)
+//        let adr = user.address;
+//         obj.push(adr);
+
+
+//      }
+//      console.log(obj)
+
+
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
+
+for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    console.log(user);
+    let usersDiv = document.createElement('div');
+
+
+    usersDiv.innerText=`${user.name} ${user.age} ${user.status} ${JSON.stringify(user.address)}`;
+    document.body.appendChild(usersDiv);
 
 }
+
+
+
 
